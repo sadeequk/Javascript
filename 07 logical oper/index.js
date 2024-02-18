5bc188a60af07d8a07eae878bfe55c285af2da71
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-18 22:28:10 +0500

    Add files via upload

---
+++ 07 logical oper/index.js
@@ -0,0 +1,27 @@
+//(---------------------------------- Logical operator------------------------------------)
+// Logical operators perform logical operations and return a boolean value, either true or false. For example,
+// logical and &&
+// var x = 5;
+// var y = 3;
+// console.log(x < 6 && y < 5);
+// console.log(x < 6 || y < 5);
+
+// console.log(true && true);
+// console.log(true || true);
+// console.log(!true);
+// console.log(!false);
+
+// console.log(true || false);
+// console.log(false && false); .......................
+// console.log(true || false);
+// console.log(false || true);
+
+// console.log(!false);
+// console.log(false);
+
+var x = 5;
+var y = 7;
+// console.log(x < y || y < x);
+// console.log(x != y);
+// console.log((x = y));.......
+// console.log(x == y);........
