37a418cb57557a255729cf4db9bacc36e340df6d
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-18 22:27:42 +0500

    Add files via upload

---
+++ 06 assg operaters/index.js
@@ -0,0 +1,91 @@
+// -------------Assignment Operator--------------
+// = ,* =, +=, -=, /=, *=, %=
+
+// let x = 2;
+// let y = 2;
+// console.log((x = y));
+
+// let x = 2;
+// let y = 2;
+// console.log(x == y);
+
+// let x = 2;
+// let y = "2";
+// console.log(x === y);
+
+// let x = 100;
+// let y = 24;
+// console.log((x += y));
+
+// let x = 100;
+// let y = "24";
+// console.log((x += y));
+// x+=y --->>> x = x+y ---->>  x = 100 +  "24" ---->15 --only concatinate it coz both have different data types
+//only cancatenate in += condition , otherwise not
+
+// let x = 100;
+// let y = 4;
+// console.log((x -= y));
+
+// let x = 100;
+// let y = "ahmad";
+// console.log((x -= y));
+
+// let x = 100;
+// let y = 3.6;
+// console.log((x -= y));
+
+// let x = 100;
+// let y = "24";
+// console.log((x -= y));
+
+// let x = 100;
+// let y = 5;
+// console.log((x *= y));
+
+// let x = 100;
+// let y = "5";
+// console.log((x *= y));
+
+// let x = 100;
+// let y = "ahmad";
+// console.log((x *= y));
+
+// let x = "ahmad";
+// let y = "ali";
+// console.log((x += y));
+
+// let x = "ahmad";
+// let y = "ali";
+// console.log((x -= y));
+
+// let x = "ahmad";
+// let y = "ad";
+// console.log((x -= y));
+
+// let x = "ahmad";
+// let y = "ad";
+// console.log((x *= y));
+
+// let x = "ahmad";
+// let y = "ad";
+// console.log((x /= y));
+
+// let x = 4;
+// let y = 4;
+// console.log((x /= y));
+
+// let x = 4;
+// let y = "4";
+// console.log((x /= y));
+
+// let x = 4;
+// let y = "ahmad";
+// console.log((x /= y));
+
+// var isTeacher = false;
+// console.log(typeof isTeacher);
+// var isStudestunt = "true";
+// console.log(typeof isStudent);
+// var isStudestunt = true;
+// console.log(typeof isStudent);
