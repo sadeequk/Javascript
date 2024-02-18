ba2a23c7bbdc831ffc0e21d30516a515c787022b
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-18 22:24:05 +0500

    Add files via upload

---
+++ 03 datatype/index.js
@@ -0,0 +1,115 @@
+//   ------------------------- DATA TYPES------------------------------------
+
+// 1) Number ------12345
+// 2) string------- "" --''-- ``(backtick)
+// 3) boolean------- "True"-- "False"
+// 4) BigInT------ above 2*1o^63
+// 5)  Null-------null
+// 6) Undefined------ example--- let y;
+// 7) SYMBOL -----Unique identifier
+// 8) object---- key value pairs
+// for example
+// let a={key :value}
+// let car={name:"toyota", model: 2022,}
+
+// */
+
+// console.log("sadeeq ullah");
+
+// 1)  Number
+// let x = 10;
+// console.log(typeof x);
+// var phoneNumber = "+92 (345)-5143101";
+// var unit1 = 5;
+// var unit2 = 5000;
+// var unit3 = 000005;
+
+// console.log(unit1, unit2, unit3, phoneNumber);
+// console.log(typeof unit1, typeof unit2, typeof unit3, typeof phoneNumber);
+
+// var digit1 = 0.000005;
+// var digit2 = 1.5;
+// console.log(digit1, digit2);
+
+// Converting string into different datatypes
+// var ageS = "24";
+// var ageN = Object(ageS);
+// var ageN = Number(ageS);
+// var ageN = Boolean(ageS);
+// console.log(ageN, typeof ageN);
+
+// 2) String
+// let x = "Sadeeq ullah khan";
+// let x = `sadeeq`;
+// let x = "sadeeeq ullah";
+// let x = ""; ----------important
+// console.log(typeof x);
+
+var string = "My name is SADEEQ ULLAH";
+// console.log(string);
+// console.log(string.toLowerCase);
+// console.log(string.toLowerCase());
+// console.log(string.toUpperCase());
+// console.log(string.toLocaleUpperCase());
+var string2 = "         Myn    ameis   SADEEQULLAH         ";
+// console.log(string2.trim()); // remove leading and trailing whitespaces Character s(e.g., spaces, tabs, newline characters)
+// console.log(string2);
+// console.log(string.trim());
+// console.log(string2.length); //white spaces will be counted in the length of the string
+// console.log(string.length);
+// console.log(string[3]);
+// console.log(string2[4]);
+// console.log(string2[4]);
+// console.log(string2[16]); //the white space will be counted
+// var lastindex = string2.length - 1;
+// console.log(string2[lastindex]);
+// console.log(string.startsWith("M")); // it will return boolean value which may be true or false
+// console.log(string.startsWith("My"));
+// console.log(string.endsWith("H"));
+// console.log(string2.endsWith("H")); // it will return false the string 2 is ende  with white space
+// console.log(string2.endsWith("")); // it will return true
+// console.log(string2.endsWith(" ")); // it will return true
+
+// console.log(string.substr("1")); //subtract first letter from the string
+// console.log(string2.substr("1")); //subtract first letter from the string
+
+// var num = 10;
+// console.log(typeof num);
+// var strNum = string(num);
+// console.log(typeof strNum);
+
+//  3) boolean
+// var sadeeq = false;
+// console.log(typeof sadeeq);
+var sadeeq = true;
+console.log(typeof sadeeq);
+// let x = 3;
+// let y = 4;
+// let z = x > y;
+// console.log(typeof z);
+
+// 4) bigInt
+// let x = (2 * 10) ^ 100;------ important
+// console.log(typeof x);
+
+// 5) null
+// let x = null;--------important
+// console.log(typeof x);
+
+// myValue = null;
+// if (myValue === null) {
+//   console.log("null");
+// } else {
+//   console.log(" not null.");
+// }
+
+// 6) undefined
+// let y;
+// console.log(typeof y);
+
+// 7) Object
+// let x = { car: "toyota", model: 2020, color: "gray" };
+// console.log(typeof x);
+
+// let x = (2 * 10) ^ 200;
+// document.write(typeof x);
