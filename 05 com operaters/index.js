caf20b20cdeaa4449048c3f141081a042a4c2683
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-18 22:27:05 +0500

    Add files via upload

---
+++ 05 com operaters/index.js
@@ -0,0 +1,72 @@
+
+// -------------------Comparesion Operators--------------
+// == or ===
+// <=
+// >
+// <
+// <=
+// !== or !=
+
+// Examples
+
+// let x = 5;
+// let y = 5;
+// console.log(x == y);
+
+// let x = 5;
+// let y = 4;
+// console.log(x == y);
+
+// let x = 5; number
+// let y = "5"; string
+// console.log(x == y); result is true bcoz
+//  Double Equal only check the values of both the variables (does not check the data type of variables)
+
+// let x = 4;
+// let y = "4";
+// console.log(x == y);
+
+//  now triple equal
+
+// let x = 4;
+// let y = "4";
+// console.log(x === y);
+// Check the values of both variable and also check the datatypes of both variables.
+// If values and datatypes are matched, it will return true.
+
+// let x = 4;
+// let y = 1 + 3;
+// console.log(x === y);
+
+// let x = 4;
+// let y = 2;
+// console.log(x === y);
+
+// let x = 4;
+// let y = 5;
+// console.log(x <= y);
+
+// let x = 4;
+// let y = 5;
+// console.log(x >= y);
+
+// let x = 4;
+// let y = 5;
+// console.log(x < y);
+
+// let x = 4;
+// let y = 5;
+// console.log(x > y);
+
+// let a = 4;
+// let b = 3;
+// console.log(a != b);
+
+// let x = 4;
+// let y = "4";
+// console.log(x !== y);
+//  it will also check the datatype of both the variables
+
+// let x = 4;
+// let y = 4;
+// console.log(x != y);
