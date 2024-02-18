084ce98745b69f8c06d6f7cf9fbb922b4f05e472
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-18 22:23:11 +0500

    Add files via upload

---
+++ 01 introduction/index.js
@@ -0,0 +1,51 @@
+// console.log("hellow world");
+// console.log("hellow");
+
+/*introduction*/
+//   variables   --- are containers for storing data or values
+
+// var x = 10;
+// {
+//   var x = 2;
+// }
+// console.log(x);
+// the output should be 2
+
+// let x = 10;
+// {
+//   let x = 2;
+// }
+// console.log(x);
+// the output should be 10
+
+// var x = 2;
+// Now x is 2
+// var x = 3;
+// Now x is 3
+
+// With let, redeclaring a variable in the same block is NOT allowed:
+// var x = 2;   // Allowed
+// let x = 3;   // Not allowed
+
+// {
+// let x = 2;   // Allowed
+// let x = 3;   // Not allowed
+// }
+
+// {
+// let x = 2;   // Allowed
+// var x = 3;   // Not allowed
+// }
+
+// Redeclaring a variable with let, in another block, IS allowed:
+// let x = 2;   // Allowed
+
+// {
+// let x = 3;   // Allowed
+// }
+
+// {
+// let x = 4;    // Allowed
+// }
+
+
