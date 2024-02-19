800f3d93e6b43d87d8460edf34d97f0e48679a26
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-19 23:19:09 +0500

    Add files via upload

---
+++ 09 Conditionl statements/index.js
@@ -0,0 +1,66 @@
+console.log("hellow world");
+// conditional statments
+// if - else- else if - switch
+
+//if conditional statmete
+// console.log((x = 4));
+// if (x > 10) {
+//   console.log("x is greater then 10");
+// }
+
+// console.log((x = 4));
+// if (x > 10) {
+//   console.log("x is greater then 10");
+// } else {
+//   console.log("x is less the 10");
+// }
+
+// if (24 > 10) {
+//   console.log("true");
+// }
+
+// if (24 < 10) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// let x = 32;
+
+// if (x < 10) {
+//   console.log("the given number is less then 10");
+// } else if (x > 100 || x <= 500) {
+//   console.log("the given number lies between 100 and 500");
+// } else {
+//   console.log(" greater  then 50");
+// }
+
+let x = 32;
+
+if (x < 10) {
+  console.log("the given number is less then 10");
+} else if (x > 100 && x <= 500) {
+  console.log("the given number lies between 100 and 500");
+} else {
+  console.log(" greater  then 50");
+}
+
+// let x = 222;
+
+// if (x < 10) {
+//   console.log("the given number is less then 10");
+// } else if (x > 10 || x <= 50) {
+//   console.log("the given number lies between 10 and 50");
+// } else {
+//   console.log(" greater  then 50");
+// }
+
+// let x = 222;
+
+// if (x < 10) {
+//   console.log("the given number is less then 10");
+// } else if (x > 10 && x <= 50) {
+//   console.log("the given number lies between 10 and 50");
+// } else {
+//   console.log(" greater  then 50");
+// }
