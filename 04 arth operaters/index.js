589ac45e3dadb1d8b2dd9c0196195328447e2bfb
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-19 23:18:27 +0500

    Add files via upload

---
+++ 04 arth operaters/index.js
@@ -0,0 +1,91 @@
+//   ------------------------- OPERATORS------------------------------
+//  Arthmetic operater + - *  /  % ( modulus) reminder
+
+// -------------ADDITION------------
+
+// let x = 2;
+// let y = 3;
+// let z = x + y;
+// console.log(z);
+
+// let x = 2;
+// let y = 3;
+// var z = x + y;
+// console.log(z);
+
+// let x = 2;
+// y = 3;
+// z = x + y;
+// console.log(z);
+
+// let x = 3;
+// let x = 4;
+// let y = 3;
+// let z = y + x;
+// console.log(z);
+
+// let x = 3;
+// x = 4;
+// let y = 3;
+// let z = y + x;
+// console.log(z);
+
+// var x = 3;
+// var x = 4;
+// var y = 6;
+// var z = x + y;
+// console.log(z);
+
+// var x = 3;
+// var x = 4;
+// var y = 6;
+// let z = x + y;
+// console.log(z);
+
+// let x = "5" + 2 + 3;
+// console.log(x);
+
+// let x = 2 + 3 + "5";
+// console.log(x);
+
+// var num1 = 20,
+//   num2 = 10,
+//   num3 = 5,
+//   num4 = 3;
+
+// console.log("SUM", num1 + num2);
+// var sum = num1 + num2;
+// console.log("SUM", sum);
+
+// console.log("SUBTRACTION", num1 - num2);
+// var sub = num1 - num2;
+// console.log("SUBTRACTION", sub);
+
+// Multiplication
+// console.log("Multiply", num1 * num2);
+// var mul = num1 * num2;
+// console.log("Multiply", mul);
+
+// Division
+// console.log("Divide", num1 / num3);
+// var div = num1 / num3;
+// console.log("Divide", div);
+
+//------------MODULUS------------
+// let x = 30;
+// let y = 6;
+// let z = x % y;
+// console.log(z);
+
+//if the divisor value is greater then the divident it will simply give the divident value in output
+//for example
+let x = 30;
+let y = 89;
+let z = x % y;
+console.log(z);
+
+//------------DIVISION------------
+// let X = 7;
+// let y = 3;
+// let z = 7 / 3;
+// console.log(z);
