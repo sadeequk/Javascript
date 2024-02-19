1acd8bfa6933a2f3ac8bbe20d236a4048fec85f1
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-19 23:21:33 +0500

    Add files via upload

---
+++ 12 do while loop/index.js
@@ -0,0 +1,8 @@
+// do while loop
+//Similar to the while loop, but it will always execute the block of code at least once because the condition is checked after the loop.
+
+let i = 0;
+do {
+    console.log(i);
+    i++;
+} while (i < 5);
