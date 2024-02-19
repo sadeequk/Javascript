634677098e72a926d6df30afbf7abdd5fd779495
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-19 23:19:28 +0500

    Add files via upload

---
+++ 10 for loop/index.js
@@ -0,0 +1,10 @@
+// ----------------------------For Loop------------------
+// The for loop is used when you know how many times you want to execute a block of code. It consists of three parts: initialization, condition, and increment (or decrement).
+
+// for (i = 0; i <= 300; i++) {
+//   console.log(i);
+// }
+
+for (i = 300; i >= 0; i--) {
+  console.log(i);
+}
