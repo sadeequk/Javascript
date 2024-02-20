3308cde568fcd1e453f6778f32050d438d3a442a
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-20 23:40:20 +0500

    Add files via upload

---
+++ 14 functions/index.js
@@ -0,0 +1,56 @@
+// function
+// block of code which perform some specific function
+// function may be written in 2 differernt form
+
+
+let a=30
+let b=40
+let c=50
+let d=66
+// function sadeeq(xyx ,b){
+//     console.log(a+b)
+// }
+// sadeeq(30 ,80)
+
+
+// function sadeeq(){
+//     let a=4;
+//     let b=70;
+//     let add=a+b;
+//     let multiply = a * b;
+//     let divide = a / b;
+
+// // return(add);
+// return{add,multiply,divide};
+// }
+// console.log(sadeeq())
+
+
+
+// let a=30
+// let b=40
+// arrow function
+let sadeeq =()=>{
+    console.log("my name is xyz");
+};
+sadeeq();
+
+
+
+let add =()=>{
+    console.log(a+b);
+};
+add();
+add();
+add();
+add();
+add();
+add();
+add();
+
+
+
+let sum =(a,b) =>{
+    console.log(a+b)
+}
+sum();
