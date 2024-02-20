b69bd18a40bbda9313b286dce37ab7f4b961ac78
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-20 23:41:06 +0500

    Add files via upload

---
+++ 17 array/index.js
@@ -0,0 +1,29 @@
+// In JavaScript, an array is a data structure that allows you to store multiple values of different data types in a single variable. 
+// Arrays are one of the fundamental data types in JavaScript and are often used for storing collections of data,
+//  such as a list of numbers, strings, objects, or a combination of these
+
+//Creating an Array
+
+// let fruit=[]  //empty array
+let fruit=["mango","apple", "Banana",];
+let colors=["red", "blue", "green","white"];
+
+//Acessing Elements
+
+// console.log(fruit[2])
+// console.log(colors[0])
+
+// Modifying Elements
+// colors[0]="pink";
+// console.log(colors)
+
+// length
+// console.log(colors.length) //output os 4
+
+// Adding Elements
+// colors.push("black");
+// console.log(colors)   //let colors=["red", "blue", "green","white" "black"];
+
+//Removing Elements
+colors.pop("green"); ///pop elements from the last of the array
+console.log(colors)
