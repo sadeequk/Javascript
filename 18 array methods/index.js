4d401db2a86e41fd6474d10e6ef4c39eef15879b
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-20 23:41:57 +0500

    Add files via upload

---
+++ 18 array methods/index.js
@@ -0,0 +1,20 @@
+//  push array method
+
+// through this method we can push an element to Array
+// for example
+// let fruit=["apple","banana","mango"]
+// console.log(fruit);
+// fruit.push("angoor");
+// console.log(fruit);
+
+//pop array method
+
+// through this method we can remove an element from an Array
+// let fruit=["apple","banana","mango"]
+// fruit.pop()
+// console.log(fruit); ///it will remove the last element
+
+
+// lenght of an array 
+let fruit=["apple","banana","mango"];
+console.log(fruit.length);
