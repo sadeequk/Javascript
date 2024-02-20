ce6903af61fef26aa22ab0f7b56bfa695cae1e59
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-20 23:40:52 +0500

    Add files via upload

---
+++ 16 arrow function/index.js
@@ -0,0 +1,59 @@
+// ARROW FUNCTIONS 
+
+// Arrow functions, introduced in ECMAScript 6 (ES6), provide a concise and more expressive way to write functions in JavaScript. 
+// They are particularly useful for defining shorter functions, 
+
+
+// Basic Syntax:
+// An arrow function is defined using a simpler syntax with the => arrow operator. The basic syntax is as follows:
+
+// const functionName = (parameters) => {
+//     // Function body
+//     return result; // Optional
+// };
+
+
+
+// const add = (a, b) => {
+//     return a + b;
+// }
+// console.log( add (10 , 30));
+
+
+
+
+// function sadeeq(){
+//     let a=4;
+//     let b=70;
+//     let add=a+b;
+//     let multiply = a * b;
+//     let divide = a / b;
+
+// // return(add);
+// return{add,multiply,divide};
+// }
+// console.log(sadeeq())
+
+
+// arrow function without parameter
+
+// let sadeeq=()=>{
+//     let a=4;
+//     let b=70;
+//     let add=a+b;
+//     let multiply = a * b;
+//     let divide = a / b;
+
+// // return(add);
+// return{add,multiply,divide};
+
+// }
+// console.log(sadeeq())
+
+
+
+// arrow function with parameter
+const add = (a, b) => {
+    return a + b;
+};
+console.log(add(30,60));
