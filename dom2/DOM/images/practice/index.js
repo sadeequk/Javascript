f0344232d27ec5a7575d1cb690eaf32331d787a9
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-21 21:14:23 +0500

    Add files via upload

---
+++ dom2/DOM/images/practice/index.js
@@ -0,0 +1,2 @@
+// document.getElementById("heading").innerHTML="sadeeq ulllah khan"
+document.getElementById("demo").innerHTML="Date :"+ Date()
---
+++ dom2/DOM/index.js
@@ -0,0 +1,10 @@
+// var box = document.querySelector(".box-circle");
+var box = document.getElementsByClassName("box-circle").innerHTML;
+console.log(box);
+// console.log(boxxx);
+
+// // to change the css style of the Element
+// box.style.backgroundColor = "blue";
+
+// box.classList;
+// // console.log(box)
