6deda73fc79f9aeb6edeb7f6515d9990611f24f7
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-21 21:13:27 +0500

    Add files via upload

---
+++ array method push pop/index.js
@@ -0,0 +1,12 @@
+// PUSH POP
+
+const student = [1, 2, 3, 4, '55', 66]
+//   Push(): Add an element at the end of the array
+//   Pop(): Remove an element from the end of the array
+
+
+// let newStudent = student.pop()
+
+console.log(student.length-1) //To find the last index of an element in an array
+console.log(student[student.length-1]) //To find the element present on the last index of the array
+console.log(student[1])
