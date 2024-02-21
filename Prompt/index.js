ccd644e7fc4b88012b8b7a7d8a93c55685511eac
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-21 21:14:42 +0500

    Add files via upload

---
+++ Prompt/index.js
@@ -0,0 +1,41 @@
+// var name = prompt("What is your Name");
+// var age = prompt("What is your age in years");
+// aged = Number(age);
+// numed = Number(name);
+// var z = aged + numed
+// var y=confirm
+// (z);
+// document.write("You age in days is ", age * 365);
+// console.log("You age in weeks is ", age * 52);
+
+// alert("Welcome to Javascript");
+// var userResponse = confirm("Are you sure you want to delete this?");
+// if (userResponse == true) {
+//   console.log("Your item has beeen deleted");
+// } else {
+//   console.log("Item NOT deleted");
+// }
+
+// Alert
+// alert('Welcome to Javascript');
+
+// ALERT
+// alert('Welcome to Class 4');
+
+// PROMPT
+// the value entered by user is always a STRING
+// var firstName = prompt("What is your first name");
+// var lastName = prompt("What is your last name?");
+// var age = prompt("What is your age?");
+// console.log(firstName, lastName, age);
+// console.log(typeof firstName, typeof lastName, typeof age);
+
+// CONSOLE
+// console.log("This is a LOG");
+// console.info("This is an INFO");
+// console.error("This is an ERROR");
+// console.warn("This is an WARNING");
+
+// var intro = "Welcome to Javascript";
+// console.log(intro);
+// console.log("Azmat", intro);
