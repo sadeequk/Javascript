236792e9895f71685d61328f818c1f786d6709cf
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-23 23:40:04 +0500

    Add files via upload

---
+++ statement/statement/index.js
@@ -0,0 +1,102 @@
+/*
+ * VARIABLES
+ * Are containers for storing data/values
+ */
+
+/**
+ * Datatypes
+ * 1) Number ----->> 1, 2, 3, integers
+ * 2) String -----> '', "", ``,
+ * 3) Boolean =----->> True, false
+ * 4) BigInt ----->> 2*10^63
+ * 5) NULL -----> null
+ * 6) UNDEFINED ----->> let y;
+ * 7) SYMBOL ----->> unique idetifier
+ * 8) OBJECT----->>> Key value pairs ----->>> let a =  {key: value}
+ * let car = {name: "Toyota", model: 2022, color:'Yello', }
+ */
+
+// const z;
+// console.log(y)
+
+/**
+ * Operators
+ * 1) Arithmetic Operators
+ * 2) Comparison Operators
+ * 3) Assignment Operations
+ * 4) Logical Operators
+ * 5) Bitwise Operators
+ */
+
+/**
+ * Arithmetic Operators:
+ * +, -, /, *, %
+ */
+
+// console.log(y%x)
+
+/**
+ * Comparison Operators
+ * 1) == or ===
+ * 2) <=
+ * 3) >=
+ * 4)!== or !=
+ */
+
+// console.log(x == y) // Check the values of both the variables
+// console.log(x === y)
+/**
+ * Check the values of both variable and also check the datatypes of both variables.
+ * If values and datatypes are matched, it will return true.
+ */
+
+/**
+ * <= Operator ----->> Values are either equal or the left side value is less than the right side value
+ */
+
+// console.log(x<=y)
+
+/**
+ * >= Operator ---->> Values are either equal or the left side value is greater than the right side value
+ */
+
+// console.log(y>=x)
+
+// console.log(x!==y)
+
+/**
+ * Assignment Operations ---->>
+ * =, +=, -=, /=, *=, %=
+ */
+
+// let x = "100";
+// x = 15
+// var y = 15;
+// x+=y --->>> x = x+y ---->>  x = 100 + '15' ---->>>> 10015 ==>> concate ==>>
+// x+=y ---->>> x+y
+// console.log(x+=y)
+// console.log(x-=y)
+/**
+ * It will look at the first value,
+ * if it's integer, then the second value's datatype will be changed according to the first one
+ */
+
+// console.log(x*=y)
+// console.log(x/=y)
+// console.log(x%=y)
+
+// console.log("hello"+ x + "akdlfkasdf;lk")
+
+/**
+ * LOGICAL OPERATORS
+ * AND OPERATOR --->>> &&
+ * OR OPERATOR ----->>> ||
+ * NOT OPERATOR ----->> !
+ */
+
+var isTeacher = false;
+console.log(typeof isTeacher);
+// var isStudestunt = "true";
+// console.log(typeof isStudent);
+// var isStudestunt = true;
+// console.log(typeof isStudent);
