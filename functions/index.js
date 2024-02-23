20af5eeca53d4eab996ce42e3cbdd69e3de92606
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-02-23 23:40:40 +0500

    Add files via upload

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+/**
+ * Functions: A block of code that can be reused when needed instead of writing it again and again
+ */
+
+// let a = 20
+// let b = 10
+// let c = a+b;
+// let d = 30
+// let e = 50
+// let f = d+e
+
+function sum(a, b){
+console.log(a+b)
+}
+sum(10,20)
+
+
+/**
+ * Arrow function  ======>>> () => {}
+ */
+
+// const sum = () => {
+
+// }
