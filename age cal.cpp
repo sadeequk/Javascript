8dda661bfd8dcee2c40a42eb9389dfff0a067640
Sadeeq Ullah <101932287+sadeequk@users.noreply.github.com>
2024-03-02 23:13:46 +0500

    Add files via upload

---
+++ age cal.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+	int age;
+	cout<<"enter your age"<<endl;
+	cin>>age;
+	if(age>=18){
+		cout<<"eligible for vote"<<endl;
+	}
+	else
+	cout<<"not eligible for vote"<<endl;
+	return 0;
+}
